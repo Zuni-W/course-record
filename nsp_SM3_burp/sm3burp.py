1f5b23bf49193295938ddf09b54c4a888795ede0
Nishiping <1559056303@qq.com>
2022-07-25 12:38:03 +0800

    sm3burp

---
+++ nsp_SM3_burp/sm3burp.py
@@ -0,0 +1,23 @@
+import os
+from subprocess import *
+
+count=0
+outlist = []
+for i in range(2**19):
+    cmd = ['echo',str(i),'|','openssl','dgst','-sm3']
+    p = Popen(' '.join(cmd),shell=True,stdout=PIPE)
+    p.wait()
+    output, unused_err = p.communicate()
+    output = output.decode()[-8:-1]
+    if output in outlist:
+        count+=1
+        #print(outlist.index(output))
+    else:
+        outlist.append(output)
+
+print(count)
+#2**16 24041 16bit
+#2**16 1928 20bit
+#2**16 104 24bit
+#2**16 4 28bit
+#2**19 521  32bit
